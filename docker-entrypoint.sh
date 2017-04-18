f9fc0fa0475765132a29a7d75733bf7a107a36c3
ling-rain <915961521@qq.com>
2017-04-18 09:44:47 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 if [ "$1" = 'kafka-server-start.sh' ]; then
 
-    sed -i -r 's|zookeeper.connect=localhost:2181|zookeeper.connect=zookeeper:2181|g' /opt/kafka/config/server.properties
+#    sed -i -r 's|zookeeper.connect=localhost:2181|zookeeper.connect=zookeeper:2181|g' /opt/kafka/config/server.properties
+    sed -i -r 's|zookeeper.connect=localhost:2181|zookeeper.connect=$ZOOKEEPER_SERVER:2181|g' /opt/kafka/config/server.properties
 
     exec "$@"
 fi
